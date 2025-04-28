2a6ff1cf4073dd03d1fc7a7f06af12cd7271a003
paulmelero <paul.melero@backmarket.com>
2025-04-28 05:28:22 +0200

    chore: Switch to lightningcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+
   devtools: { enabled: true },
+
   runtimeConfig: {
     contentIslandSecretToken: process.env.CONTENT_ISLAND_SECRET_TOKEN,
   },
+
   css: ['@/assets/styles/global.css'],
-  modules: ['@nuxt/eslint', '@nuxt/fonts', 'nitro-cloudflare-dev'],
-  fonts: {
-    processCSSVariables: true,
-  },
 
   experimental: {
     payloadExtraction: true,
@@ -19,9 +18,23 @@ export default defineNuxtConfig({
     preset: 'cloudflare-module',
   },
 
+  vite: {
+    css: {
+      transformer: 'lightningcss',
+    },
+    build: {
+      cssMinify: 'lightningcss',
+    },
+  },
+
   routeRules: {
     '/': { prerender: true },
     '/about': { prerender: true },
     '/posts/**/**': { prerender: true },
+  },
+
+  modules: ['@nuxt/eslint', '@nuxt/fonts', 'nitro-cloudflare-dev'],
+  fonts: {
+    processCSSVariables: true,
   },
 });
